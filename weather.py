dbdaf30caceb10b76c5bbdf9ce6c1285681bc08d
alundyz <88989587+alundyz@users.noreply.github.com>
2023-12-14 21:30:05 -0500

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,14 +1,42 @@
 import requests
+import argparse
+from datetime import datetime, timedelta
 
-url = "https://api.tomorrow.io/v4/timelines"
+def get_weather(city, day):
+    url = "https://api.tomorrow.io/v4/timelines"
+    end_date = datetime.utcnow() + timedelta(days=day)
+    start_date = end_date - timedelta(days=1)
+    
+    querystring = {
+        "location": city,
+        "fields": ["temperature", "cloudCover"],
+        "units": "imperial",
+        "timesteps": "1d",
+        "startTime": start_date.isoformat() + "Z",
+        "endTime": end_date.isoformat() + "Z",
+        "apikey": "rbv146AnfUsuHquseBxaCZkvdvGWSall"
+    }
 
-querystring = {
-"location":"33, -84",
-"fields":["temperature", "cloudCover"],
-"units":"imperial",
-"timesteps":"1d",
-"apikey":"rbv146AnfUsuHquseBxaCZkvdvGWSall"}
+    response = requests.request("GET", url, params=querystring)
+    data = response.json().get("data", {}).get("timelines", [])
+    
+    if data:
+        weather_data = data[0].get("intervals", [])
+        result = [{"StartTime": interval["startTime"], "Values": interval["values"]} for interval in weather_data]
+        return result
 
-response = requests.request("GET", url, params=querystring)
-print(response.text)
-print(type(response.text))
+    return []
+
+def main():
+    parser = argparse.ArgumentParser(description="Get weather information for a city and day")
+    parser.add_argument("--city", type=str, help="City name", required=True)
+    parser.add_argument("--day", type=int, help="Number of days", required=True)
+    args = parser.parse_args()
+
+    weather_data = get_weather(args.city, args.day)
+    print(args.city, "Weather in ",args.day,"days")
+    for interval in weather_data:
+        print(interval)
+
+if __name__ == "__main__":
+    main()
